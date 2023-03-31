60a32179562b4089809c52ddd9fc509872c8c2b1
ftCLI <ftCLI@proton.me>
2023-03-31 09:05:17 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="PyFontConverter",
-    version="1.0.1",
-    description="Command line tools to convert font files.",
+    version="1.0.2",
+    description="Command line font converter.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     author="ftCLI",
@@ -17,11 +17,11 @@ setuptools.setup(
     include_package_data=True,
     entry_points={"console_scripts": ["font-converter=font_converter.font_converter:cli"]},
     install_requires=[
-        "fonttools==4.39.2",
-        "skia-pathops==0.7.4",
-        "cffsubr==0.2.9.post1",
-        "click==8.1.3",
-        "pathvalidate==2.5.2",
+        "fonttools>=4.39.2",
+        "skia-pathops>=0.7.4",
+        "cffsubr>=0.2.9.post1",
+        "click>=8.1.3",
+        "pathvalidate>=2.5.2",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
